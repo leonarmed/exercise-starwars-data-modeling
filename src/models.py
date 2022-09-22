dbfec63849bcf5ed719912c1630355f5ff009219
Leonardo Medina <leonarmed@gmail.com>
2022-09-22 23:44:43 +0000

    Data model of starwars

--- src/models.py
+++ src/models.py
@@ -1,33 +1,73 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy import render_er
+from sqlalchemy.sql import func
+
+time_created = Column(DateTime(timezone=True), server_default=func.now())
+time_updated = Column(DateTime(timezone=True), onupdate=func.now())
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Usuarios(Base):
+    __tablename__ = 'usuario'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    lname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    favorites_key = Column(Integer, ForeignKey('favorites.id'))
+    favorites = relationship("Favorites")
 
-class Address(Base):
-    __tablename__ = 'address'
+class Planetas(Base):
+    __tablename__ = 'planetas'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    diameter = Column(String(250), nullable=False)
+    gravity = Column(String(250), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    favorites_key = Column(Integer, ForeignKey('favorites.id'))
+    favorites = relationship("Favorites")
+
+class Personajes(Base):
+    __tablename__ = 'personajes'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)
+    height = Column(String(250), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    favorites_key = Column(Integer, ForeignKey('favorites.id'))
+    favorites = relationship("Favorites")
 
-    def to_dict(self):
+    def serializer(self):
         return {}
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    personajes = relationship("Personajes")
+    usuarios = relationship("Usuarios")
+    planetas = relationship("Planetas")
+    user_id = Column(Integer, ForeignKey('usuario.id'))
+    pers_id = Column(Integer, ForeignKey('personajes.id'))
+    planet_id = Column(Integer, ForeignKey('planetas.id'))
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
